687b117a89acbfb229800c2a2938448911f24b3b
Luiggi Abdiel <luiggiabdieldev@gmail.com>
2022-11-14 22:49:38 -0300

    feat: navbar responsiveness, new section main

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,11 @@
 import { IoIosArrowForward,IoIosArrowBack  } from 'react-icons/io'
 
-import { Navbar } from "./Navbar"
+import Console from '../assets/console.svg'
+import Rating from '../assets/rating.svg'
+import Xbox from '../assets/xbox.svg'
+
+import { Navbar } from './Navbar'
+import {GamesGenres} from './GamesGenres'
 
 import Styles from './Main.module.scss'
 
@@ -11,7 +16,7 @@ export function Main() {
                 <Navbar />
                 <div className={Styles.boxText}>
                     <h1>MultiVersus</h1>
-                    <span>Now officially free to play for all Xbox users.</span>
+                    <span className={Styles.subTitle}>Now officially free to play for all Xbox users.</span>
                     <button className={Styles.btnGetInNow}>Get in now!</button>
                 </div>
                 <div className={Styles.icons}>
@@ -27,6 +32,42 @@ export function Main() {
                     </p>
                 </div>
             </div>
+
+            <section className={Styles.sectionGames}>
+                <div className={Styles.sectionRatingText}>
+                    <h2>Why eXStore?</h2>
+                    <span>We are the official Xbox partner in Indonesia that provides official video games.</span>
+                </div>
+                <div className={Styles.containerRating}>
+                    <div>
+                        <img src={Console} alt="" />
+                        <span className={Styles.descriptionRating}>5000+ video games available</span>
+                    </div>
+                    <div>
+                        <img src={Rating} alt="" />
+                        <span className={Styles.descriptionRating}>Trusted by 100.000+ customers</span>
+                    </div>
+                    <div>
+                        <img src={Xbox} alt="" />
+                        <span className={Styles.descriptionRating}>Available for all Xbox consoles</span>
+                    </div>
+                </div>
+
+                <div className={Styles.gamesGenres}>
+                    <div className={Styles.genresText}>  
+                        <h2>Games Genres</h2>
+                        <a href='#' className={Styles.seelAllGenres}>See all genres</a>
+                    </div>
+                     <div>
+                        <GamesGenres />
+                        <GamesGenres />
+                        <GamesGenres />
+                        <GamesGenres />
+                        <GamesGenres />
+                        <GamesGenres />
+                     </div>
+                </div>
+            </section>
         </>
     )
 }
